17feef667a8ae695ac5f1cd8b10cdb0aa0546996
RAJAPRIYA19 <rajapriyasathesh1111@gmail.com>
2025-08-18 20:59:28 +0530

    initial commit

---
+++ B/config.php
@@ -0,0 +1,14 @@
+<?php
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "oat";
+$port = 3306;
+
+$conn = new mysqli($servername, $username, $password, $dbname, $port);
+
+if ($conn->connect_error) 
+{
+  die("Connection failed: " . $conn->connect_error);
+}
+?>
